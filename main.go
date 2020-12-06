20eb7a4f528693b0877de7e4a7926a8ed7360df3
Jonathan Pulsifer <jonathan@pulsifer.ca>
2020-12-06 22:15:02 +0000

    try to listen to all interfaces
    Signed-off-by: Jonathan Pulsifer <jonathan@pulsifer.ca>

--- main.go
+++ main.go
@@ -1,23 +1,3 @@
-// Copyright © 2017 Jonathan Pulsifer
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 package main
 
 import (
@@ -32,6 +12,8 @@ import (
 )
 
 func init() {
+	// set default log level
+	log.SetLevel(log.InfoLevel)
 	// register dnsCounter metric
 	prometheus.MustRegister(dnsCounter)
 }
@@ -41,15 +23,29 @@ func main() {
 		iface   string
 		promisc bool
 		snaplen int
+		verbose bool
 	)
 
 	// cli flags
 	flag.StringVar(&iface, "interface", "", "interface to listen on")
 	flag.BoolVar(&promisc, "promisc", true, "promiscuous mode")
+	flag.BoolVar(&verbose, "verbose", false, "enable debug logging")
 	flag.IntVar(&snaplen, "snaplen", 65536, "packet snap length")
 	flag.Parse()
 
-	if iface == "" {
+	if verbose {
+		log.SetLevel(log.DebugLevel)
+	}
+
+	if len(flag.Args()) == 0 {
+		log.WithFields(log.Fields{
+			"promiscuous": promisc,
+			"snaplen":     snaplen,
+		}).Info("No flags specified, using defaults")
+	}
+
+	switch strings.ToLower(iface) {
+	case "", "any", "all":
 		// find network interfaces
 		devices, err := pcap.FindAllDevs()
 		if err != nil {
@@ -59,52 +55,45 @@ func main() {
 		// get the first device with an IP address
 		for _, device := range devices {
 			if len(device.Addresses) > 0 {
-				iface = device.Name
-				break
+				handle, err := pcap.OpenLive(device.Name, int32(snaplen), promisc, pcap.BlockForever)
+				if err != nil {
+					log.Fatal(err)
+				}
+				defer handle.Close()
+				log.Infof("Listening on device: %s", device.Name)
+				go listenToInterface(handle)
 			}
 		}
+	default:
+		handle, err := pcap.OpenLive(iface, int32(snaplen), promisc, pcap.BlockForever)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer handle.Close()
+		log.Infof("Listening on device: %s", iface)
+		listenToInterface(handle)
 	}
+}
 
-	if len(flag.Args()) == 0 {
-		log.WithFields(log.Fields{
-			"interface":   iface,
-			"promiscuous": promisc,
-			"snaplen":     snaplen,
-		}).Info("No flags specified, using defaults")
-	}
-
-	// set up interface handler
-	log.Infof("Listening on device: %s", iface)
-	handle, err := pcap.OpenLive(iface, int32(snaplen), promisc, pcap.BlockForever)
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer handle.Close()
-
+func listenToInterface(handle *pcap.Handle) {
 	// packet vars
 	var (
-		eth     layers.Ethernet
-		ip4     layers.IPv4
-		ip6     layers.IPv6
-		dst     string
-		src     string
-		tcp     layers.TCP
-		udp     layers.UDP
-		dns     layers.DNS
-		payload gopacket.Payload
+		eth layers.Ethernet
+		ip4 layers.IPv4
+		ip6 layers.IPv6
+		dst string
+		src string
+		tcp layers.TCP
+		udp layers.UDP
+		dns layers.DNS
 	)
 
-	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &udp, &dns, &payload)
+	parser := gopacket.NewDecodingLayerParser(layers.LayerTypeEthernet, &eth, &ip4, &ip6, &tcp, &udp, &dns)
 	decodedLayers := []gopacket.LayerType{}
 
 	packets := gopacket.NewPacketSource(handle, handle.LinkType())
 	for packet := range packets.Packets() {
-		err := parser.DecodeLayers(packet.Data(), &decodedLayers)
-		// errors are for packets we don't care about
-		if err != nil {
-			log.Debugln(err)
-		}
-
+		parser.DecodeLayers(packet.Data(), &decodedLayers)
 		// start dns logger
 		dnsLog := log.WithFields(log.Fields{})
 
@@ -128,12 +117,11 @@ func main() {
 				for _, query := range dns.Questions {
 					// type, class, opcode, rcode
 					dnsCounter.WithLabelValues(query.Type.String(), query.Class.String(), dns.OpCode.String(), dns.ResponseCode.String()).Inc()
-					dnsLog = dnsLog.WithFields(log.Fields{
+					dnsLog.WithFields(log.Fields{
 						"class": query.Class.String(),
 						"name":  string(query.Name),
 						"type":  query.Type.String(),
-					})
-					dnsLog.Info("QUERY")
+					}).Info("QUERY")
 				}
 				for _, answer := range dns.Answers {
 					dnsCounter.WithLabelValues(answer.Type.String(), answer.Class.String(), dns.OpCode.String(), dns.ResponseCode.String()).Inc()
@@ -157,16 +145,20 @@ func main() {
 					case "PTR":
 						dnsLog = dnsLog.WithFields(log.Fields{"value": string(answer.PTR)})
 					case "SOA":
-						dnsLog = dnsLog.WithFields(log.Fields{"mname": string(answer.SOA.MName)})
-						dnsLog = dnsLog.WithFields(log.Fields{"rname": string(answer.SOA.RName)})
-						dnsLog = dnsLog.WithFields(log.Fields{"serial": int32(answer.SOA.Serial)})
-						dnsLog = dnsLog.WithFields(log.Fields{"refresh": int32(answer.SOA.Refresh)})
-						dnsLog = dnsLog.WithFields(log.Fields{"retry": int32(answer.SOA.Retry)})
-						dnsLog = dnsLog.WithFields(log.Fields{"expire": int32(answer.SOA.Expire)})
-						dnsLog = dnsLog.WithFields(log.Fields{"minimum": int32(answer.SOA.Minimum)})
+						dnsLog = dnsLog.WithFields(log.Fields{
+							"mname":   string(answer.SOA.MName),
+							"rname":   string(answer.SOA.RName),
+							"serial":  int32(answer.SOA.Serial),
+							"refresh": int32(answer.SOA.Refresh),
+							"retry":   int32(answer.SOA.Retry),
+							"expire":  int32(answer.SOA.Expire),
+							"minimum": int32(answer.SOA.Minimum),
+						})
 					case "MX":
-						dnsLog = dnsLog.WithFields(log.Fields{"name": string(answer.MX.Name)})
-						dnsLog = dnsLog.WithFields(log.Fields{"preference": int16(answer.MX.Preference)})
+						dnsLog = dnsLog.WithFields(log.Fields{
+							"name":       string(answer.MX.Name),
+							"preference": int16(answer.MX.Preference),
+						})
 					case "TXT":
 						var txts []string
 						for _, txt := range answer.TXTs {
@@ -174,10 +166,12 @@ func main() {
 						}
 						dnsLog = dnsLog.WithFields(log.Fields{"txt": strings.Join(txts, " ")})
 					case "SRV":
-						dnsLog = dnsLog.WithFields(log.Fields{"priority": int16(answer.SRV.Priority)})
-						dnsLog = dnsLog.WithFields(log.Fields{"weight": int16(answer.SRV.Weight)})
-						dnsLog = dnsLog.WithFields(log.Fields{"port": int16(answer.SRV.Port)})
-						dnsLog = dnsLog.WithFields(log.Fields{"name": string(answer.SRV.Name)})
+						dnsLog = dnsLog.WithFields(log.Fields{
+							"priority": int16(answer.SRV.Priority),
+							"weight":   int16(answer.SRV.Weight),
+							"port":     int16(answer.SRV.Port),
+							"name":     string(answer.SRV.Name),
+						})
 					}
 					dnsLog.Info("ANSWER")
 				}
